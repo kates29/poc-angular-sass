089a1bc8f50b4ec8668a7048f5017c7db28b5514
Samaniego Revilla <k.samaniego.revilla@accenture.com>
2018-06-12 12:25:59 -0400

    Added first page complete

--- src/app/container-components/dashboard/dashboard.component.ts
+++ src/app/container-components/dashboard/dashboard.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  apps_array:any[] = [1,2,3,4,5,6,7,8,9,'*',0,'a','f','as'];
+  apps_array:any[] = [
+    {"name":"Saldos Y Transferencias", "icon": "access_time"},
+    {"name":"Banca Móvil", "icon": "access_time"},
+    {"name":"Mi App", "icon": "access_time"},
+    {"name":"PagoRUT", "icon": "access_time"},
+    {"name":"Clave Tarjeta", "icon": "access_time"},
+    {"name":"Cambiar Clave App", "icon": "access_time"},
+    {"name":"Otra Fx", "icon": "access_time"},
+    {"name":"Seguros", "icon": "access_time"},
+    {"name":"Inversiones", "icon": "access_time"},
+    {"name":'Avance Tarjetas', "icon": "access_time"},
+    {"name":"Dudas existenciales", "icon": "access_time"},
+    {"name":'Más...', "icon": "access_time"}
+  ];
 
   constructor() { }
 
